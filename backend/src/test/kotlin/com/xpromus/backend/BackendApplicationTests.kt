01c335ae934a105724c444daef62cd2f93784f6f
Christopher Hilgner <hilgi45@gmail.com>
2024-06-03 23:40:32 +0200

    Initial commit

---
+++ backend/src/test/kotlin/com/xpromus/backend/BackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.xpromus.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
